ff9801223908677e97a2596391a13dc3791464fe
skosari <skosari@protonmail.com>
2020-08-30 13:32:15 -0700

    Working schema + Lodash

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@ const express = require('express'); //npm install express --save
 //    {  function   }
 const { graphqlHTTP } = require('express-graphql'); //npm install graphql --save
 
-const schema = require('./schema/schema.js');
+const schema = require('./schema/schema');//Import our schema for use in app.use
 
 const app = express();
 
 //localhost:4000/graphql
 app.use('/graphql', graphqlHTTP({
   graphiql: true,
-  schema: schema
+  schema: schema //Telling our app that we are using the schema.js file for our scheme. Since this is ES6 we can omit writting schema:schema and just write schema
 })) 
 
 //npm app.js
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,39 +3,41 @@ const graphql = require('graphql');
 
 //Access our necessary graphql classes
 const  {
-  GraphQLObjectType,
-  GraphQLID,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLSchema
-
+  GraphQLObjectType, //making new query objects like rootquery
+  GraphQLSchema, //export our root query with
+  GraphQLID, //for ID types but we didnt use
+  GraphQLString, 
+  GraphQLInt
 } = graphql
+
 
 //Create Types
 const UserType = new GraphQLObjectType({
   name: 'User',
   description: 'Documentation for user...',
-  fields: () => ({
-    id: {type: GraphQLString},//GraphQLID will cast the input as a unique ID
+  fields: () => ({//Notice its wrapped in () to return everything inside
+    //We use functions to define everything before its run and prevent undefined errors
+    id: {type: GraphQLString},//GraphQLID will cast the input as a unique ID but for simplicity we used GraphQLString
     name: {type: GraphQLString},//GraphQLString will cast the input as a string
     age: {type: GraphQLInt}//GraphQLInt will cast the input as integer
   })
 });
 
-//RootQuery the first query
-const RootQuery = new GraphQLObjectType({
+//RootQuery the first query - the getting of data
+const RootQuery = new GraphQLObjectType({//RootQuery is also what we export
   name: 'RootQueryType',
   description: 'Description',
   fields: {
     user: {
-      type: UserType,
-      args: {
-        id: {type: GraphQLString}
-      },
-      resolve(parent, args) {
-        //We resolve with date
-        //get and return data from a datasource
-
+      type: UserType,//we create userType and its fields in another const
+      args: {id: {type: GraphQLString}}, //Arguments we pass along when we want to retrieve data from our user:
+      resolve(parent, args) { // a function that tells graphql where to get the information - get and return data from a datasource
+        let user = {//We add a user and return it then we can access it in graphiQL
+          id: "234",
+          name: "Siamak",
+          age: 39
+        }
+        return user;
       }
     }
   }
@@ -45,3 +47,4 @@ module.exports = new GraphQLSchema({
   query: RootQuery
 })
 
+//A schema is a map of all the data and how they relate/interact
