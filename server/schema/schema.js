9ce89054b303ce66a67ccce83825a70251550e3a
skosari <skosari@protonmail.com>
2020-09-03 11:27:19 -0700

    pre relationships

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,27 +2,36 @@
 const graphql = require('graphql');
 
 //lodash - a library of utility functions we can use on our arrays/data
+//npm i --save lodash     install inside your apps root folder
 var _ = require('lodash');
 
 //Dummy Data
 var usersData = [
-  {id:1, name:'Siamak', age: 39},
-  {id:2, name:'Kambiz', age: 51},
-  {id:3, name:'Jenelle', age: 39},
-  {id:4, name:'Myles', age: 6},
-  {id:5, name:'Everrete', age: 6},
-  {id:6, name:'Leia', age: 2},
+  {id:1, name:'Siamak',   age: 39, driving: 'yes'},
+  {id:2, name:'Kambiz',   age: 51, driving: 'yes'},
+  {id:3, name:'Jenelle',  age: 39, driving: 'yes'},
+  {id:4, name:'Myles',    age: 6,  driving: 'no'},
+  {id:5, name:'Everrete', age: 6,  driving: 'no'},
+  {id:6, name:'Leia',     age: 2,  driving: 'no'},
 ]
-
+var hobbyData = [
+  {id:'1', title:'RC Cars', description: 'battery operated small scale radio controlled cars'},
+  {id:'2', title:'RC Boats', description: 'battery operated small scale radio controlled boats'},
+  {id:'3', title:'RC Planes', description: 'battery operated small scale radio controlled planes'},
+]
+var postData = [
+  {id: '1', comment: 'first post'},
+  {id: '2', comment: 'second post'}
+]
 
 //Access our necessary graphql classes
 const  {
   GraphQLObjectType, //making new query objects like rootquery
   GraphQLSchema, //export our root query with
-  GraphQLID, //for ID types but we didnt use
-  GraphQLString, 
-  GraphQLInt,
-  GraphQLNonNull
+  GraphQLID, //for ID types - when inputing data make sure to wrap id in '' but you can omit the "" when querying data
+  GraphQLString, //string types
+  GraphQLInt, //numbers
+  GraphQLNonNull //required types
 } = graphql
 
 
@@ -34,7 +43,27 @@ const UserType = new GraphQLObjectType({
     //We use functions to define everything before its run and prevent undefined errors
     id: {type: GraphQLInt},//GraphQLID will cast the input as a unique ID but for simplicity we used GraphQLString
     name: {type: GraphQLString},//GraphQLString will cast the input as a string
-    age: {type: GraphQLInt}//GraphQLInt will cast the input as integer
+    age: {type: GraphQLInt},//GraphQLInt will cast the input as integer
+    driving: {type: GraphQLString}
+  })
+});
+
+const HobbyType = new GraphQLObjectType({
+  name: 'Hobby',
+  description: 'Hobby Description',
+  fields: () => ({
+    id: {type: GraphQLID},
+    title: {type: GraphQLString},
+    description: {type: GraphQLString}
+  })
+});
+
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  description: 'Post',
+  fields: () => ({
+    id: {type: GraphQLID},
+    comment: {type: GraphQLString}
   })
 });
 
@@ -47,13 +76,23 @@ const RootQuery = new GraphQLObjectType({//RootQuery is also what we export
       type: UserType,//we create userType and its fields in another const
       args: {id: {type: GraphQLInt}}, //Arguments we pass along when we want to retrieve data from our user:
       resolve(parent, args) { // a function that tells graphql where to get the information - get and return data from a datasource
-        // let user = {//We add a user and return it then we can access it in graphiQL
-        //   id: "234",
-        //   name: "Siamak",
-        //   age: 39
-        // }
-        // return user;
         return _.find(usersData, {id: args.id})//return in userData the args.ida - notice {id: args.id} was defined under user.args
+      }
+    },
+    hobby: {
+      type: HobbyType,
+      args: {id: {type: GraphQLID}},
+      resolve(parent, args) {
+        return _.find(hobbyData, {id: args.id}) //Make hobbyData and notice the camel case
+      }
+    },
+    post: {
+      type: PostType,
+      description: 'Post new item',
+      args: {id: {type: GraphQLID}},
+      resolve(parent,args){
+        //return data (post data)
+        return _.find(postData, {id: args.id})
       }
     }
   }
